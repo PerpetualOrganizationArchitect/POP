538b0537834c0f57d9940b62852ff2fbb0f68f4f
hudsonhrh <hudsonhrh7@gmail.com>
2025-09-27 16:02:57 -0400

    chore: format

--- src/libs/RoleResolver.sol
+++ src/libs/RoleResolver.sol
@@ -16,29 +16,26 @@ library RoleResolver {
      * @param roleIndices Array of role indices (0, 1, 2, etc.)
      * @return hatIds Array of corresponding Hat IDs from the Hats Protocol
      */
-    function resolveRoleHats(
-        OrgRegistry orgRegistry,
-        bytes32 orgId,
-        uint256[] memory roleIndices
-    ) internal view returns (uint256[] memory hatIds) {
+    function resolveRoleHats(OrgRegistry orgRegistry, bytes32 orgId, uint256[] memory roleIndices)
+        internal
+        view
+        returns (uint256[] memory hatIds)
+    {
         uint256 length = roleIndices.length;
         hatIds = new uint256[](length);
-        
+
         for (uint256 i = 0; i < length; i++) {
             hatIds[i] = orgRegistry.getRoleHat(orgId, roleIndices[i]);
         }
     }
-    
+
     /**
      * @notice Validates that all role indices are within bounds
      * @param roleIndices Array of role indices to validate
      * @param maxRoles Maximum number of roles in the organization
      * @return valid True if all indices are valid
      */
-    function validateRoleIndices(
-        uint256[] memory roleIndices,
-        uint256 maxRoles
-    ) internal pure returns (bool valid) {
+    function validateRoleIndices(uint256[] memory roleIndices, uint256 maxRoles) internal pure returns (bool valid) {
         uint256 length = roleIndices.length;
         for (uint256 i = 0; i < length; i++) {
             if (roleIndices[i] >= maxRoles) {
@@ -47,7 +44,7 @@ library RoleResolver {
         }
         return true;
     }
-    
+
     /**
      * @notice Ensures array is not empty (for critical roles that must be assigned)
      * @param roleIndices Array to check
